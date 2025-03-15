63495ce2f3a4994a33c1a5bc6a3166429165da62
Breno <breno202nemezes@gmail.com>
2025-03-15 17:45:16 -0300

    quest1

---
+++ Resumo_prova.js
@@ -0,0 +1,37 @@
+const somar = (a,b,c) =>{
+    return a+b+c
+}
+const multi = (a,b) =>{
+    return a*b
+}
+
+//função de alta ordem
+const executar = (fn, ...params)=>{
+    return `${fn(...params)}`
+    }
+
+
+const r1 = executar(soma,4,5,6)
+console.log(r1)
+
+
+//RECURSIVIDADE
+
+const soma = (lista)=>{
+    if (lista.length == 0){
+        return 0
+    }else{
+        const [x, ...xs] = lista
+        return x + soma(xs)
+    }
+}
+
+//QUESTÃO 1 Lista 4
+
+const enesimo = (n)=>{
+    if (n == 1){
+        return 3
+    }else{
+        return 2 * enesimo(n-1)
+    }
+}

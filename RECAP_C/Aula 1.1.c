a516a46b40a7b7c13040c20deec352981a098c30
Breno <breno202nemezes@gmail.com>
2025-03-17 21:05:27 -0300

    Recap_C

---
+++ RECAP_C/Aula 1.1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    printf("Retomando aprendizado em C\n");
+    system("pause");//pausa o programa antes de fechar
+    return 0;
+}
---
+++ RECAP_C/Aula_1.2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main(){
+    //VER COMO DEBUGAR O CODIGO
+    printf("Mensagem 1\n");
+    printf("Mensagem 2\n");
+    printf("Mensagem 3\n");
+    printf("Mensagem 4\n");
+    system("pause");
+}

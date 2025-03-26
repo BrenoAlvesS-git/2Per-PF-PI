dcfd64e5f470762063fcc6b80d78cee2b581769d
Breno <breno202nemezes@gmail.com>
2025-03-26 17:47:12 -0300

    leitura de arquivo

---
+++ RECAP_C/abrindoARQ.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main(){
+    FILE *arq;
+    char frase[30];
+    arq = fopen("aquivo.txt","r");
+    if (arq == NULL){
+        printf("Arquivo não pode ser aberto");
+        exit(1);
+    }
+    if (fgets(frase, sizeof(frase), arq) != NULL){
+        puts(frase);
+    } else {
+        printf("Erro ao ler o arquivo\n");
+    }
+    
+    fclose(arq);
+}

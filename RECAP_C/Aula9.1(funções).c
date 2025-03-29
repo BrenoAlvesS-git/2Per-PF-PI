bc6e1c746802bd49801ab9f24b942174445358b2
Breno <breno202nemezes@gmail.com>
2025-03-29 14:06:59 -0300

    Funções

---
+++ RECAP_C/Aula9.1(funções).c
@@ -0,0 +1,38 @@
+float maior(float num1, float num2){
+    if (num1>num2){
+        return num1;
+    }else{
+        return num2;
+    }
+}
+//FUNÇÃO DE VETOR
+void imprime(int v[], int tam){
+    for(int x = 0; x< tam, x++){
+        printf("%d ", v[x]);
+    }
+}
+//FUNÇÃO DE MATRIZ
+void imprime(int m[][4], int n){
+    for(int x = 0; x< n, x++){
+        for(int y = 0; y< 4; y++){
+            printf("%d ",m[x][y]);
+        }
+    }
+}
+//OUTRA FUNÇÃO DE VETOR
+void imprime5(int v[5]){
+    for(int x = 0; x< 5, x++){
+        printf("%d ", v[x]);
+    }
+}
+
+
+
+void main(){
+    float x,y,m;
+    printf("Digite dois numeros: ");
+    scanf("%f%f",&x,&y);
+    m = maior(x,y);
+    printf("O maior numero é: %.2f ou %.2f",m, maior(x,y));
+}
+

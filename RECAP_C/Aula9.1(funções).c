1ecb096187540bf51df86d0dcc0f4da7b53de2dd
Breno <breno202nemezes@gmail.com>
2025-03-29 16:15:19 -0300

    correções

--- RECAP_C/Aula9.1(funções).c
+++ RECAP_C/Aula9.1(funções).c
@@ -1,3 +1,6 @@
+//É POSSÍVEL CHAMAR FUNÇÕES DEPOIS DO main(), inicando ela antes
+//float maior(float num1, float num2); caso se usasse isso, a função maior poderia estar após o fim do main()
+
 float maior(float num1, float num2){
     if (num1>num2){
         return num1;
@@ -7,7 +10,7 @@ float maior(float num1, float num2){
 }
 //FUNÇÃO DE VETOR
 void imprime(int v[], int tam){
-    for(int x = 0; x< tam, x++){
+    for(int x = 0; x< tam; x++){
         printf("%d ", v[x]);
     }
 }
@@ -21,7 +24,7 @@ void imprime(int m[][4], int n){
 }
 //OUTRA FUNÇÃO DE VETOR
 void imprime5(int v[5]){
-    for(int x = 0; x< 5, x++){
+    for(int x = 0; x< 5; x++){
         printf("%d ", v[x]);
     }
 }

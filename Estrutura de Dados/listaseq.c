da8fe42bb3937e38cb6d819a29e9c41287f0aa24
Breno <breno202nemezes@gmail.com>
2025-05-28 19:56:26 -0300

    ed

--- Estrutura de Dados/listaseq.c
+++ Estrutura de Dados/listaseq.c
@@ -28,10 +28,17 @@ else
 
 /*Função que faz inserção ordenada no vetor*/
 int insereOrd(int *vetor){
-    if (quantElem < maximo){
-        printf("\nDigite um numero inteiro: ");
-        
+    if (quantElem >= maximo){
+        printf("Vetor cheio, não é possivel adicionar nenhum valor");
+        return -1;
     }
+    int x = *vetor - 1;
+    while (x >= 0 && vetor[x] > quantElem){
+        vetor[x+1] = vetor[x];
+        x--;
+    }
+    vetor[x+1]= quantElem;
+    
 }
 
 /*Função que faz pesquisa sequencial*/
@@ -40,6 +47,24 @@ for(int x = 0; x < quantElem; x++)
     if(elemento == vetor[x])
        return x;
 return -1;
+}
+
+/*Pesquisa binaria*/
+int pesqBin(int *vetor, int elemento,int valor){
+    int inicio = 0;
+    int fim = elemento - 1;
+    while(inicio <= fim){
+        int meio = (inicio + fim)/2;
+    
+    if (vetor[meio] == valor){
+        return meio;
+    }else if(vetor[meio]< valor){
+        inicio = meio+1;
+    }else{
+        fim = meio - 1;
+    }
+}
+    return -1;
 }
 
 /*Função que exibe os elementos do vetor*/
@@ -71,7 +96,8 @@ lista = criaLista();
 
 do{
         printf("\n\n\nEscolha uma opcao\n1 - Inserção nao ordenada\n2 - Pesquisa sequencial");
-        printf("\n3 - Remocao nao ordenada\n9 - Exibir lista \n0 - Sair do programa");
+        printf("\n3 - Remocao nao ordenada\n4-inserçao ordenada\n5-Pesquisa binaria\n");
+        printf("9 - Exibir lista \n0 - Sair do programa");
         printf("\nDigite sua opcao: ");
         scanf("%d",&op);
         switch(op){
@@ -95,6 +121,16 @@ do{
                 else
                     printf("\nFalha na remoção do elemento.");
                 break;
+        case 4:
+            printf("Digite o numeor que deseja adicionar: ");
+            scanf("%d",*lista);
+            resp = insereOrd(*lista);
+            break;
+        case 5:
+            printf("Que numero deseja pesquisar: ");
+            scanf("%d", &aux);
+            resp = pesqBin(*lista, quantElem, aux);
+            break;
         case 9: mostraVetor(lista);
                 break;
         case 0: printf("\nEncerrando programa.");
